bfea733de4b2823680898baa95b20c92e4b053de
storm <reviakinea@gmail.com>
2021-02-17 15:47:06 +0200

    Константы преднагрева

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1507,9 +1507,9 @@
 #define PREHEAT_1_TEMP_BED     70
 #define PREHEAT_1_FAN_SPEED     0 // Value from 0 to 255
 
-#define PREHEAT_2_LABEL       "ABS"
-#define PREHEAT_2_TEMP_HOTEND 240
-#define PREHEAT_2_TEMP_BED    110
+#define PREHEAT_2_LABEL       "PETG"
+#define PREHEAT_2_TEMP_HOTEND 230
+#define PREHEAT_2_TEMP_BED    75
 #define PREHEAT_2_FAN_SPEED     0 // Value from 0 to 255
 
 /**
